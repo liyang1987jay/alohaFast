fce1cd4d30d46f41e36061abe8a30e2cdb74c859
liyang <liyang1987jay@163.com>
2017-10-15 21:59:18 +0800

    Spring的ApplicationContext的持有者,可以用静态方法的方式获取spring容器中的bean

---
+++ aloha-core/src/main/java/com/aloha/alohaFast/base/util/SpringContextHolder.java
@@ -0,0 +1,48 @@
+package com.aloha.alohaFast.base.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liyang
+ * @packageName com.aloha.alohaFast.base.util
+ * @fileName SpringContextHolder
+ * @description Spring的ApplicationContext的持有者,可以用静态方法的方式获取spring容器中的bean
+ * @date 2017-10-15 下午9:46
+ *  Copyright (c) 2014-2017 山东安合信达电子科技有限公司 版权所有 
+ *  shandong aloha CO.,LTD. All Rights Reserved. 
+ */
+@Component
+public class SpringContextHolder implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        assertApplicationContext();
+        return applicationContext;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String beanName) {
+        assertApplicationContext();
+        return (T) applicationContext.getBean(beanName);
+    }
+
+    public static <T> T getBean(Class<T> requiredType) {
+        assertApplicationContext();
+        return applicationContext.getBean(requiredType);
+    }
+
+    private static void assertApplicationContext() {
+        if (SpringContextHolder.applicationContext == null) {
+            throw new RuntimeException("applicaitonContext属性为null,请检查是否注入了SpringContextHolder!");
+        }
+    }
+}
